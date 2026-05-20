2d3f1a1d05687cc795dd731eaddb5866c3608db4
chethanteja-kumar <chethantejakumar66@gmail.com>
2026-05-21 01:07:54 +0530

    Update success message in server response

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend Running Successfully");
+  res.send("Backend Running Successfully!!");
 });
 
 app.listen(5000, () => {
